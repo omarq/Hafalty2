5966c832807db5d855bdbc5cb5f64eb30592dc47
Omar Qadan <omar.qadan@gmail.com>
2019-05-07 15:37:11 +0300

    configuer application project

--- Haflaty.Application/Interfaces/Mapping/IHaveCustomMapping.cs
+++
@@ -1,9 +0,0 @@
-﻿using AutoMapper;
-
-namespace Haflaty.Application.Interfaces.Mapping
-{
-    public interface IHaveCustomMapping
-    {
-        void CreateMappings(Profile configuration);
-    }
-}
